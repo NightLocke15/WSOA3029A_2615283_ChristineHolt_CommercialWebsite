cd2a82e448e8ee5e0e86e367353448c0806e99f4
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-09-23 16:58:52 +0200

    nav link fix

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,20 +1,13 @@
 const menu = [
-    {name: "Home", href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/index.html"},
-    {name: "Recipes", href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/recipes/index.html"},
-    {name: "Articles", href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/articles/index.html"},
-    {name: "About", href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/about/index.html"}
+    {name: "Home", href: "/index.html"},
+    {name: "Recipes", href: "/recipes/index.html"},
+    {name: "Articles", href: "/articles/index.html"},
+    {name: "About", href: "/about/index.html"}
 ]
 
 const navList = document.querySelector(".navigation-buttons");
 
 export function navigationBar(currentPage) {
-    let navItems = menu.reduce(function (values, item) {
-        if (!values.includes(item.name)) {
-            values.push(item.name);
-        }
-        return values;
-    }, []);
-
     let buttons = menu.map(function (menuItem) {
         return `<a class="nav-button" id="${menuItem.name}" href="${menuItem.href}">${menuItem.name}</a>`
     }).join("");
@@ -24,8 +17,6 @@ export function navigationBar(currentPage) {
     const buttonsList = document.querySelectorAll(".nav-button");
 
     buttonsList.forEach(function (button) {
-        
-
         if (button.id != currentPage) {
             button.setAttribute("id", "otherButton");
         }
