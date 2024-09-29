102287ab27e190cb47672c990e8736f2bdba39a9
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-09-29 15:55:35 +0200

    Link fix

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -3,21 +3,21 @@ const homePageSections = [
         id: "recipes",
         name: "Just Recipes",
         description: "No other stuff. Find recipes by fellow cooks and bakers in our extensive archives. Use, Like and Share the recipe, easily find what you need without having to dig through anything unnecessary!",
-        href: "/recipes/index.html",
+        href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/recipes/index.html",
         button: "Find Recipes"
     },
     {
         id: "articles",
         name: "Articles",
         description: "Do you want to read more about food? Have a look at our articles, backed by facts (and a few opinions).",
-        href: "/articles/index.html",
+        href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/articles/index.html",
         button: "Read Articles"
     },
     {
         id: "post",
         name: "Post a Recipe",
         description: "Want to add one of your delicious recipes to our archives? Shoot us a message with the ingredients, instructions and any notes and have your recipe between other great ones!",
-        href: "/recipes/postARecipe.html",
+        href: "/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/recipes/postARecipe.html",
         button: "Post a Recipe"
     }
 ]
