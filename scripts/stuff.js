c894c078f7ecefdc664b498c97e5013478dbe394
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-10-04 08:58:04 +0200

    Final recipe stuff hopefully

--- scripts/stuff.js
+++ scripts/stuff.js
@@ -6,4 +6,18 @@ function recipe() {
     let myCurrentRecipeString = localStorage.getItem('currentRecipe');
     let myCurrentRecipe = JSON.parse(myCurrentRecipeString);
     console.log(myCurrentRecipe[0].recipeElements.ingredients);
+
+    recipeContent.innerHTML = `<div class="pageTitle">${myCurrentRecipe[0].title}</div>
+    <section class="recipeCont">
+        <div class="verticalLine"></div>
+        <section class="recipeContent">
+            <img src="${myCurrentRecipe[0].image}" class="recipeImage" alt="" title="${myCurrentRecipe[0].imageRef}">
+            <p>Ingredients:</p>
+            ${myCurrentRecipe[0].recipeElements.ingredients}
+            <p>Instructions:</p>
+            ${myCurrentRecipe[0].recipeElements.instructions}
+            ${myCurrentRecipe[0].recipeElements.notes}
+            ${myCurrentRecipe[0].recipeElements.reference}
+        </section>
+    </section>`;
 }
