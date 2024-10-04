9547aa2a5e67b8782f59084e35dff8c72225678d
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-10-04 08:48:01 +0200

    test 3

--- scripts/stuff.js
+++ scripts/stuff.js
@@ -1,7 +1,9 @@
 recipe();
 
+const recipeContent = document.querySelector(".recipeContentSection");
+
 function recipe() {
     let myCurrentRecipeString = localStorage.getItem('currentRecipe');
     let myCurrentRecipe = JSON.parse(myCurrentRecipeString);
-    console.log(myCurrentRecipe[0].ingredients);
+    console.log(myCurrentRecipe[0].recipeElements.ingredients);
 }
