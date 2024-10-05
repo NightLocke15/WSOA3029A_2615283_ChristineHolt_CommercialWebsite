f8a027e09df83c22b19e67ab9a936959ed4cd70b
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-10-05 21:24:58 +0200

    Assignment 3 Final

--- scripts/articles.js
+++ scripts/articles.js
@@ -2,12 +2,12 @@
 const articles = [
     {
         name: "Essay: Critical Analysis of the Effectiveness of Data Visualisations",
-        description: "",
+        description: "This essay goes in depth on the effectiveness of the data visualisations in an NBC News article.",
         href: `/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/articles/essay.html`
     },
     {
         name: "Fruit Sugar Content and Which Ones are Better for a Low Sugar Diet",
-        description: "",
+        description: "This article talks on the sugar content of fruit and how all fruits are healthy, but it shows which fruits are better based on whether someone would like to eat a lower sugar diet.",
         href: `/WSOA3029A_2615283_ChristineHolt_CommercialWebsite/articles/article1.html`
     }
 ]
@@ -21,12 +21,14 @@ injectArticles();
 function injectArticles() {
     //map the info into a variable
     let articleLinks = articles.map(function (article) {
-        return `<article class="articleLink">
+        return `<a href="${article.href}"><article class="articleLink">
         <div class="articleTitle">${article.name}</div>
         <p>${article.description}</p>
-        </article>`
+        </article></a>`
     });
 
     //use the variable to inser the data into the html page
     articleContainer.innerHTML = articleLinks;
+
+
 }
